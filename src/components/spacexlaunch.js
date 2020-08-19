a037de69931c1df51dfc39f23b2b14cfd072b1d0
hariom <hariom@starlly.in>
2020-08-19 18:26:18 +0530

    responsive issue resolve

--- src/components/spacexlaunch.js
+++ src/components/spacexlaunch.js
@@ -39,92 +39,101 @@ export default class SpaceXLaunch extends React.Component {
                         <div className="col-md-12">
                             <h5 className="header">Filters</h5>
                         </div>
+                        <br />
                         <div className="col-md-12">
                             <div className="inputStyle">Launch Year</div>
                         </div>
-                        <div className="row">
+                        {/* <div className='container-fluid'> */}
+                        <div className="btn-container">
                             {
                                 this.state.year.map(el => {
-                                    return (<div className="col-md-6"><button type="button" className="btn btn-success fontStyle marginStyle">{el}</button></div>)
+                                    return (
+                                        <div className="col-md-6">
+                                            <button type="button" className="btn btn-success fontStyle marginStyle">
+                                                {el}
+                                            </button>
+                                        </div>
+                                    )
                                 })
                             }
                         </div>
+                        {/* </div> */}
                         <div className="col-md-12">
                             <div className="inputStyle">Sucessfull Year</div>
                         </div>
-                        <div className="row">
+                        <div className="btn-container">
                             <div className="col-md-6"><button type="button" className="btn btn-success fontStyle marginStyle">True</button></div>
                             <div className="col-md-6"><button type="button" className="btn btn-success fontStyle marginStyle">False</button></div>
                         </div>
                         <div className="col-md-12">
                             <div className="inputStyle">Sucessfull Landing</div>
                         </div>
-                        <div className="row">
+                        <div className="btn-container">
                             <div className="col-md-6"><button type="button" className="btn btn-success fontStyle marginStyle">True</button></div>
                             <div className="col-md-6"><button type="button" className="btn btn-success fontStyle marginStyle">False</button></div>
                         </div>
                     </div>
-                    <div className="col-md-10">
-                        <div className="col-md-12">
-                            {
-                                this.state.spacexData.map(el => {
-                                    return (
-                                        <div className="col-md-3" >
-                                            <div className="col-md-12 card no-padding">
-                                                <div className="imageThumbnail">
-                                                    <img src={el.links.mission_patch} className="card-img-top imgStyle" alt="..." />
+                    <div className="col-md-10 no-padding">
+                        {/* <div className="col-md-12"> */}
+                        {
+                            this.state.spacexData.map(el => {
+                                return (
+                                    <div className="col-sm-12 col-md-3 card-padding" >
+                                        <div className="col-md-12 card no-padding pt-3">
+                                            <div className="imageThumbnail">
+                                                <img src={el.links.mission_patch} className="card-img-top imgStyle" alt="..." />
+                                            </div>
+                                            <div className="detailSection">
+                                                <div className="col-md-12 spaceTitle">
+                                                    <span className="floatLeft">{el.mission_name} #{el.flight_number}</span>
                                                 </div>
-                                                <div className="detailSection">
-                                                    <div className="col-md-12 spaceTitle">
-                                                        <span className="floatLeft">{el.mission_name} #{el.flight_number}</span>
+                                                <div className="col-md-12 spaceTitle">
+                                                    <div className="col-md-6 label">
+                                                        <span className="floatLeft">Mission Ids : </span>
                                                     </div>
-                                                    <div className="col-md-12 spaceTitle">
-                                                        <div className="col-md-6 label">
-                                                            <span className="floatLeft">Mission Ids : </span>
-                                                        </div>
-                                                        <div className="col-md-6 label">
-                                                            <ul className="floatLeft">
-                                                                {
-                                                                    el.mission_id.map(mission => {
-                                                                        return <li className="no-bold">{mission}</li>
-                                                                    })
-                                                                }
-                                                            </ul>
-                                                        </div>
+                                                    <div className="col-md-6 label">
+                                                        <ul className="floatLeft">
+                                                            {
+                                                                el.mission_id.map(mission => {
+                                                                    return <li className="no-bold">{mission}</li>
+                                                                })
+                                                            }
+                                                        </ul>
                                                     </div>
-                                                    <div className="col-md-12 spaceTitle">
-                                                        <div className="col-md-6 label">
-                                                            <span className="floatLeft">Launch Year : </span>
-                                                        </div>
-                                                        <div className="col-md-6 label no-bold">
-                                                            <span className="floatLeft">{el.launch_year}</span>
-                                                        </div>
+                                                </div>
+                                                <div className="col-md-12 spaceTitle">
+                                                    <div className="col-md-6 label">
+                                                        <span className="floatLeft">Launch Year : </span>
                                                     </div>
-                                                    <div className="col-md-12 spaceTitle">
-                                                        <div className="col-md-6 label">
-                                                            <span className="floatLeft">Successfull <br /> Launch : </span>
-                                                        </div>
-                                                        <div className="col-md-6 label no-bold">
-                                                            <span className="floatLeft">{el.launch_success ? String(el.launch_success) : "false"}</span>
-                                                        </div>
+                                                    <div className="col-md-6 label no-bold">
+                                                        <span className="floatLeft">{el.launch_year}</span>
                                                     </div>
-                                                    <div className="col-md-12 spaceTitle">
-                                                        <div className="col-md-6 label">
-                                                            <span className="floatLeft">Successfull <br />
+                                                </div>
+                                                <div className="col-md-12 spaceTitle">
+                                                    <div className="col-md-6 label">
+                                                        <span className="floatLeft">Successfull <br /> Launch : </span>
+                                                    </div>
+                                                    <div className="col-md-6 label no-bold">
+                                                        <span className="floatLeft">{el.launch_success ? String(el.launch_success) : "false"}</span>
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12 spaceTitle">
+                                                    <div className="col-md-6 label">
+                                                        <span className="floatLeft">Successfull <br />
                                                                 Landing : </span>
-                                                        </div>
-                                                        <div className="col-md-6 label no-bold">
-                                                            <span className="floatLeft">{el.land_success ? String(el.land_success) : "false"}</span>
-                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6 label no-bold">
+                                                        <span className="floatLeft">{el.land_success ? String(el.land_success) : "false"}</span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    )
-                                })
-                            }
+                                    </div>
+                                )
+                            })
+                        }
 
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
