d14f3c4062b81ab68ddc8a7ae76e8c008ccf6cca
Prabhat Sharma <prabhat@starlly.in>
2020-08-20 12:52:40 +0530

    added couple of test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { FilterCard } from './components/filter-card';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/spacex launch programs/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test if filter rendered', () => {
+  const filterTestData = {
+    years: ['2020', '2019', '2018', '2017', '2016']
+  };
+
+  render(<FilterCard years={filterTestData.years}></FilterCard>)
+
+  expect(screen.queryByText('2012')).toBeNull()
+  expect(screen.getByText('2020')).toBeInTheDocument()
+  expect(screen.getByText('2019')).toBeInTheDocument()
+  expect(screen.getByText('2018')).toBeInTheDocument()
+  expect(screen.getByText('2017')).toBeInTheDocument()
+  expect(screen.getByText('2016')).toBeInTheDocument()
+  
+})
